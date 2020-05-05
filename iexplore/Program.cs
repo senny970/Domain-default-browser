e162c876b6486fff5f6b033552b4eb31ba60598f
Alexander <senny970@gmail.com>
2020-05-05 14:04:43 +0300

    Initial commit

---
+++ iexplore/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace iexplore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var AppPath = System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            string filePath = AppPath + "/browser_path.txt";
+            //Console.WriteLine(filePath);
+
+            Process p = new Process();
+            if (args.Length == 1 && File.Exists(filePath))
+            {
+                string browser;
+                StreamReader rdr = new StreamReader(filePath);
+                browser = rdr.ReadToEnd();
+                rdr.Close();
+                //Console.Write(browser);                
+                //Console.ReadKey();
+
+                //Console.WriteLine(args[0]);
+                //p.StartInfo.FileName = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+                p.StartInfo.FileName = browser;
+                p.StartInfo.Arguments = args[0].ToString();
+                p.Start();
+            }
+        }
+    }
+}
